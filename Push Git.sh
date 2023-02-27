44e60fc5c1d7ae3b064340c32f35cb9f4fad2c58
Techmokid <aj2000wa@gmail.com>
2023-02-27 16:36:31 +0800

    Correcting erroneous branch

--- Push Git.sh
+++ Push Git.sh
@@ -4,7 +4,7 @@ gnome-terminal -- sh -c '
 #!/bin/sh
 git add *
 git commit -m "Automated Git Push"
-git branch -M main
+#git branch -M main
 git push -u origin main
 
 read -t 5 -p "Press [Enter] key to finish"
