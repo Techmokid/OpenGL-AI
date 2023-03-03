ed84b4b8503d94895c544c61f984d89ebfce9ee1
Techmokid <aj2000wa@gmail.com>
2023-03-03 23:01:17 +0800

    Automated Git Push

--- OpenGL Examples/compileAll.py
+++ OpenGL Examples/compileAll.py
@@ -1,7 +1,11 @@
 import os,platform
 
 for f in os.listdir():
-    if os.path.isfile(f) and f.endswith('.cpp'):
-        command = "g++ \"" + f + "\" -IF:/freeglut/include -LF:/freeglut/lib -w -Wl,-subsystem,windows -lOpenGL32 -lglu32 -lfreeGLUT -std=c++17 -m32 -o \"" + f + "_" + platform.system() + "_" + platform.release() + ".exe\""
-        os.system(command)
-        print(command)
+	if os.path.isfile(f) and f.endswith('.cpp'):
+		command = ""
+		if (platform.system() == "Windows"):
+			command = "g++ \"" + f + "\" -IF:/freeglut/include -LF:/freeglut/lib -w -Wl,-subsystem,windows -lOpenGL32 -lglu32 -lfreeGLUT -std=c++17 -m32 -o \"" + f + "_" + platform.system() + "_" + platform.release() + ".exe\""
+		else:
+			command = "g++ \"" + f + "\" -lglut -lGLU -lGL -lpthread -std=c++17 -o \"" + f + "_" + platform.system() + "_" + platform.release() + ".out\""
+		os.system(command)
+		print(command)
