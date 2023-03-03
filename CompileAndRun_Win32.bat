a6e1b926b7b8f82ca95118e6080acfabddb086dd
Techmokid <58498119+Techmokid@users.noreply.github.com>
2023-03-03 23:12:01 +0800

    Corrected Windows

--- CompileAndRun_Win32.bat
+++ CompileAndRun_Win32.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 
-g++ *.cpp *.h -lpthread -IF:\freeglut\include -LF:\freeglut\lib -w -Wl,-subsystem,windows -lOpenGL32 -lglu32 -lfreeGLUT -std=c++17 -m32 -o AI_Server_Win32.exe
+g++ *.cpp *.h -lpthread -IF:\freeglut\include -LF:\freeglut\lib -w -Wl,-subsystem,windows -lOpenGL32 -lglu32 -lfreeGLUT -std=c++17 -o AI_Server_Win32.exe
 echo Finished Compiling. If errors were found, hitting enter will run the last known working version
 pause
 
--- OpenGL Examples/compileAll.py
+++ OpenGL Examples/compileAll.py
@@ -4,8 +4,8 @@ for f in os.listdir():
 	if os.path.isfile(f) and f.endswith('.cpp'):
 		command = ""
 		if (platform.system() == "Windows"):
-			command = "g++ \"" + f + "\" -IF:/freeglut/include -LF:/freeglut/lib -w -Wl,-subsystem,windows -lOpenGL32 -lglu32 -lfreeGLUT -std=c++17 -m32 -o \"" + f + "_" + platform.system() + "_" + platform.release() + ".exe\""
+			command = "g++ \"" + f + "\" -IF:/freeglut/include -LF:/freeglut/lib/x64 -w -Wl,-subsystem,windows -lOpenGL32 -lglu32 -lfreeGLUT -std=c++17 -o \"" + f + "_" + platform.system() + ".exe\""
 		else:
-			command = "g++ \"" + f + "\" -lglut -lGLU -lGL -lpthread -std=c++17 -o \"" + f + "_" + platform.system() + "_" + platform.release() + ".out\""
+			command = "g++ \"" + f + "\" -lglut -lGLU -lGL -lpthread -std=c++17 -o \"" + f + "_" + platform.system() + ".out\""
 		os.system(command)
 		print(command)
