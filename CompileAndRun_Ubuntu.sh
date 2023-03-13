4accbc03ff3e79ea74d09aebaa7a22fd3ed8b44d
Techmokid <aj2000wa@gmail.com>
2023-03-13 16:47:03 +0800

    Automated Git Push

--- CompileAndRun_Ubuntu.sh
+++ CompileAndRun_Ubuntu.sh
@@ -3,14 +3,14 @@
 gnome-terminal -- sh -c '
 #!/bin/bash
 
-g++ *.cpp *.h -lpthread -lglut -lGLU -lGL -std=c++17 -o OpenGLTest.out
+g++ *.cpp *.h -lpthread -lglut -lGLU -lGL -std=c++17 -o AI_Server_Ubuntu.out
 
 echo Finished Compiling. If errors were found, hitting enter will run the last known working version
 
 printf "%s " "Press enter to run program"
 read ans
 
-./OpenGLTest.out
+./AI_Server_Ubuntu.out
 
 printf "%s " "Press enter to close terminal"
 read ans
--- OpenGL SSBO Test/CompileAndRun_Ubuntu.sh
+++ OpenGL SSBO Test/CompileAndRun_Ubuntu.sh
@@ -3,7 +3,7 @@
 gnome-terminal -- sh -c '
 #!/bin/bash
 
-g++ *.cpp -lpthread -lglut -lGLU -lGL -lGLEW -lglfw -std=c++17 -fconcepts -o AI_Server_Ubuntu.out
+g++ *.cpp -lpthread -lglut -lGLU -lGL -lGLEW -lglfw -std=c++17 -fconcepts -o OpenGLTest.out
 
 echo Finished Compiling. If errors were found, hitting enter will run the last known working version
 
@@ -12,7 +12,7 @@ read ans
 
 clear
 
-./AI_Server_Ubuntu.out
+./OpenGLTest.out
 
 printf "%s " "Press enter to close terminal"
 read ans
